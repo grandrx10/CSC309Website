513f496d6595bc21f9276cb4a1e4d72079829cf6
grandrx10 <41299974+grandrx10@users.noreply.github.com>
2025-04-08 22:23:05 -0400

    GODDAMNMIT

--- frontend/src/pages/Signin/Signin.jsx
+++ frontend/src/pages/Signin/Signin.jsx
@@ -17,7 +17,7 @@ import {
 } from 'antd';
 import "./Signin.module.css"; // You can keep your custom styles if needed
 const { Title, Text } = Typography;
-const API_URL = process.env.REACT_APP_API_URL || "https://localhost:3100";
+const API_URL = process.env.REACT_APP_API_URL || "https://csc-309-website-9pvx.vercel.app";
 console.log("THIS IS MY ENV VARIABLE:")
 console.log(process.env)
 function SignIn() {
